b3fca03dd074a378d7afe44dbe8099fa5a2cffd2
rmwillow <rmwillowbis2bis@gmail.com>
2021-06-16 18:41:43 -0400

    console errors fixed

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function getGoogleMaps() {
-
-
-    fetch("https://maps.googleapis.com/maps/api/js?key=AIzaSyBtQgwtmt7aoZSZHJo2BT50rx2nqbZb8Tw&callback=initMap")
-        .then(response => response.json())
-        .then(data => console.log(data));
-}
-getGoogleMaps();
