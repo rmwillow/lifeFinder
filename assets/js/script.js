5f558360ec7dbbb2a3bfc7382da10352a47f9287
Victor Lupascu <sinajeen@gmail.com>
2021-06-18 13:52:05 -0500

    add an extra modal div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 
 // start sample modal
-var sampleModal = function () {
+var alertModal = function () {
     var htmlEl = document.querySelector('html');
-    var modalEl = document.querySelector('.modal');
+    var modalEl = document.querySelector('#alert');
     modalEl.classList.add('is-active');
     htmlEl.classList.add('is-clipped');
 };
 
 var closeModalBtn = function () {
     var htmlEl = document.querySelector('html');
-    var modalEl = document.querySelector('.modal');
+    var modalEl = document.querySelector('#alert');
     modalEl.classList.remove('is-active');
     htmlEl.classList.remove('is-clipped');
-}
+};
 
 document.querySelector('.modal-close').addEventListener('click', closeModalBtn);
 document.querySelector('.modal-background').addEventListener('click', closeModalBtn);
